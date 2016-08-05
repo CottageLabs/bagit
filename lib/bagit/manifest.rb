cf266fbe4d2907fc58412ba4b43d989edd18663e
Nevelina Aleksandrova <nevelina@cottagelabs.com>
2016-08-05 10:52:30 +0100

    more debugging

--- lib/bagit/manifest.rb
+++ lib/bagit/manifest.rb
@@ -29,6 +29,7 @@ module BagIt
       # manifest each tag file for each algorithm
       bag_files.each do |f|
         puts f
+        puts f.inspect
         rel_path = Pathname.new(f).relative_path_from(Pathname.new(bag_dir)).to_s
 
         # sha1
